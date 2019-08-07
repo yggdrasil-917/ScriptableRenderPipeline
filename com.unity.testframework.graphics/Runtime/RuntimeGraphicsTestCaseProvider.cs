eaf43f7419d400bde096aef68d6504417bcdca98
sophia <sophia@unity3d.com>
2019-08-07 12:39:17 +0200

    using webrequest and lowercase for assetbundles

--- com.unity.testframework.graphics/Runtime/RuntimeGraphicsTestCaseProvider.cs
+++ com.unity.testframework.graphics/Runtime/RuntimeGraphicsTestCaseProvider.cs
@@ -1,20 +1,71 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using UnityEngine.Networking;
 
 namespace UnityEngine.TestTools.Graphics
 {
     internal class RuntimeGraphicsTestCaseProvider : IGraphicsTestCaseProvider
     {
-       public IEnumerable<GraphicsTestCase> GetTestCases()
+        public IEnumerable<GraphicsTestCase> GetTestCases()
         {
             AssetBundle referenceImagesBundle = null;
+            string[] scenePaths;
 
-            var referenceImagesBundlePath = string.Format("{0}/referenceimages-{1}-{2}-{3}", Application.streamingAssetsPath, UseGraphicsTestCasesAttribute.ColorSpace, UseGraphicsTestCasesAttribute.Platform, UseGraphicsTestCasesAttribute.GraphicsDevice);
-            if (File.Exists(referenceImagesBundlePath))
-                referenceImagesBundle = AssetBundle.LoadFromFile(referenceImagesBundlePath);
+            // need the ToLower here for Unix path case sensitivity (android, linux, OSX, iOS)
+            // apparently unity automatically saves the asset bundle as all lower case
+            var referenceImagesBundlePath = string.Format("referenceimages-{0}-{1}-{2}",
+                UseGraphicsTestCasesAttribute.ColorSpace,
+                UseGraphicsTestCasesAttribute.Platform,
+                UseGraphicsTestCasesAttribute.GraphicsDevice).ToLower();
 
-            foreach (var scenePath in File.ReadAllLines(Application.streamingAssetsPath + "/SceneList.txt"))
+            referenceImagesBundlePath = Path.Combine(Application.streamingAssetsPath, referenceImagesBundlePath);
+
+            using (var webRequest = new UnityWebRequest(referenceImagesBundlePath))
+            {
+                var handler = new DownloadHandlerAssetBundle(referenceImagesBundlePath, 0);
+                webRequest.downloadHandler = handler;
+
+                webRequest.SendWebRequest();
+
+                while (!webRequest.isDone)
+                {
+                    // wait for response
+                }
+
+                if (string.IsNullOrEmpty(webRequest.error))
+                {
+                    referenceImagesBundle = handler.assetBundle;
+                }
+                else
+                {
+                    Debug.Log("Error loading reference image bundle, " + webRequest.error);
+                }
+            }
+
+            using (var webRequest = UnityWebRequest.Get(Application.streamingAssetsPath + "/SceneList.txt"))
+            {
+                webRequest.SendWebRequest();
+
+                while (!webRequest.isDone)
+                {
+                    // wait for download
+                }
+
+                if (string.IsNullOrEmpty(webRequest.error) || webRequest.downloadHandler.text != null)
+                {
+                    scenePaths = webRequest.downloadHandler.text.Split(
+                        new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.RemoveEmptyEntries);
+                }
+                else
+                {
+                    scenePaths = new string[] { string.Empty };
+                    Debug.Log("Scene list was not found.");
+                }
+            }
+
+            foreach (var scenePath in scenePaths)
             {
                 var imagePath = Path.GetFileNameWithoutExtension(scenePath);
 
@@ -49,7 +100,7 @@ namespace UnityEngine.TestTools.Graphics
             if (referenceImagesBundle != null)
                 referenceImage = referenceImagesBundle.LoadAsset<Texture2D>(imagePath);
 
-            output = new GraphicsTestCase( scenePath, referenceImage );
+            output = new GraphicsTestCase(scenePath, referenceImage);
 
             return output;
         }
