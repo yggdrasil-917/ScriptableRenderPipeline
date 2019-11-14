752e6b7d380245bccda93610307e31678d5b2368
Sophia Clarke <sophiaclarke93@gmail.com>
2019-11-14 14:36:51 +0100

    get srp revision from verified package version

--- .yamato/postjob.py
+++ .yamato/postjob.py
@@ -1,13 +1,47 @@
 import sys
-import requests
-url = "https://yamato-api.cds.internal.unity3d.com/jobs"
-srp_revision = sys.argv[1]
-unity_revision = sys.argv[2]
-api_key = sys.argv[3]
-branch_name = sys.argv[4]
+#import requests
+import json
+import tarfile
+
+#url = "https://yamato-api.cds.internal.unity3d.com/jobs"
+srp_revision = ""
+#unity_revision = sys.argv[2]
+#api_key = sys.argv[3]
+#branch_name = sys.argv[4]
+
+#core_package = "com.unity.render-pipelines.core"
+#External/PackageManager/Editor/
+with open('manifest.json') as f:
+  manifest = json.load(f)
+
+packages = manifest['packages']
+
+core_package = packages['com.unity.render-pipelines.core']
+
+version = core_package['minimumVersion']
+
+package_name = 'com.unity.render-pipelines.core' + '-' + version + '.tgz'
+package_path = 'External/PackageManager/Editor/'
+
+package = ""
+tar = tarfile.open(package_name, "r:*")
+for filename in tar.getnames():
+  try:
+      package_tar = tar.extractfile('package/package.json')
+      tar_data = package_tar.read()
+      package = tar_data
+  except:
+      print('ERROR: Did not find %s in tar archive' % filename)
+
+
+package_json = json.loads(package)
+repository = package_json['repository']
+srp_revision = repository['revision']
+print(srp_revision)
+
 data = '''{
   "source": {
-    "branchname": "''' + branch_name + '''",
+    "branchname": "master",
     "revision": "''' + srp_revision + '''"
   },
   "links": {
@@ -23,7 +57,7 @@ data = '''{
 
 key = 'ApiKey ' + api_key
 
-response = requests.post(url, data=data, headers={'Authorization': key})
+# response = requests.post(url, data=data, headers={'Authorization': key})
 
 if(response.ok):
     print "ok"
