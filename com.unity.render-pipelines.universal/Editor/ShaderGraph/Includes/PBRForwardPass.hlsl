5e18a58a1c7bee891cf7a89316f934cf3163fb5e
Alex Lindman <alexandral@unity3d.com>
2019-12-18 15:58:03 -0800

    use on the fly bitangent in universal PBR forward pass

--- com.unity.render-pipelines.universal/Editor/ShaderGraph/Includes/PBRForwardPass.hlsl
+++ com.unity.render-pipelines.universal/Editor/ShaderGraph/Includes/PBRForwardPass.hlsl
@@ -4,7 +4,8 @@
 #ifdef _NORMALMAP
 
 #if _NORMAL_DROPOFF_SPACE==0
-    inputData.normalWS = TransformTangentToWorld(normal, half3x3(input.tangentWS.xyz, input.bitangentWS.xyz, input.normalWS.xyz));
+    float3 bitangent = (input.tangentWS.w<0.0 ? -1.0 : 1.0) * cross(input.normalWS.xyz, input.tangentWS.xyz);
+    inputData.normalWS = TransformTangentToWorld(normal, half3x3(input.tangentWS.xyz, bitangent, input.normalWS.xyz));
 #elif _NORMAL_DROPOFF_SPACE==1
 	inputData.normalWS = TransformObjectToWorldNormal(normal);
 #elif _NORMAL_DROPOFF_SPACE==2
